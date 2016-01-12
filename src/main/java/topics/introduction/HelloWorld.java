4409ef7d2aeb4a4fa254e509c7ff80d4a5831a84
viceg <viceg@DESKTOP-676T0CB>
2016-01-12 20:34:22 +0100

    JavaDoc included

--- src/main/java/topics/introduction/HelloWorld.java
+++ src/main/java/topics/introduction/HelloWorld.java
@@ -2,9 +2,20 @@ package topics.introduction;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+/**
+ * Just to show a small example of how the project is structured
+ * @author viceg
+ */
 public class HelloWorld {
 	static Logger log = LoggerFactory.getLogger(HelloWorld.class);
 	
+	/**
+	 * Adds two integer numbers and return the result
+	 * 
+	 * @param a The first integer number to be added
+	 * @param b The second integer number to be added
+	 * @return The calculation of a+b
+	 */
 	public int sum(int a, int b) {
 		log.info("Adding two numbers");
 		return a+b;
--- src/test/java/topics/introduction/HelloWorldTest.java
+++ src/test/java/topics/introduction/HelloWorldTest.java
@@ -4,20 +4,33 @@ import static org.junit.Assert.*;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
+/**
+ * HelloWorld JUnit tests
+ * @author viceg
+ */
 public class HelloWorldTest {
 	private static HelloWorld helloWorld;
 	
+	/**
+	 * Initializes the object to perform tests
+	 */
 	@BeforeClass
 	public static void setup() {
 		helloWorld = new HelloWorld();
 	}
 	
+	/**
+	 * Adds 10 + 40 and expects the result is 50
+	 */
 	@Test
 	public void testSum() {
 		int sum = helloWorld.sum(10, 40);
 		assertEquals("The addition was not executed correctly", 50, sum);
 	}
 	
+	/**
+	 * Adds 10 + 40 and expects the result is not 49 or 51
+	 */
 	@Test
 	public void testSum2() {
 		int sum = helloWorld.sum(10, 40);
