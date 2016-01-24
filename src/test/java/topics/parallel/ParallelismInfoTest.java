ca5265024ccda526abdb312ebcb1942c61c8af79
viceg <viceg@DESKTOP-676T0CB>
2016-01-24 11:29:36 +0100

    Moving Parallelism

---
+++ src/test/java/topics/parallel/ParallelismInfoTest.java
@@ -0,0 +1,32 @@
+package topics.parallel;
+
+import java.util.concurrent.ForkJoinPool;
+
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * ParallelismInfo JUnit tests
+ * @author viceg
+ */
+public class ParallelismInfoTest {
+	private static Logger log = LoggerFactory.getLogger(ParallelismInfoTest.class);
+	
+	/**
+	 * Shows the level of parallelism of the computer
+	 */
+	@Test
+	public void executeTask() {
+	    ForkJoinPool pool = new ForkJoinPool(5); //Task pool 
+
+	    log.debug("Level of parallelism: " + 
+	    pool.getParallelism());
+	    
+	    log.debug("Available processors: " + 
+	    Runtime.getRuntime().availableProcessors()); 
+	}
+
+	  	
+
+}
